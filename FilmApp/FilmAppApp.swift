a61f629cc4adfce16cb09e997006e5538e1a214b
hasanuysaal <hsnuysaal@icloud.com>
2022-09-19 16:48:29 +0300

    Initial Commit

---
+++ FilmApp/FilmAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmAppApp.swift
+//  FilmApp
+//
+//  Created by Hasan Uysal on 19.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct FilmAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
